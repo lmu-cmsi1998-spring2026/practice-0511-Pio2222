3d62490ca08c5c49f24d3f0335cd7fdeda427c2b
Pio2222 <pongante@lion.lmu.edu>
2026-05-13 00:43:16 -0700

    added divide function

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function greetUser(name) {
-    return `Hello, ${name}!`;
+  return `Hello, ${name}!`;
 }
 
 function add(a, b) {
-    return a + b;
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
 }
 
 console.log(greetUser("Pio"));
 console.log(add(2, 3));
+console.log(multiply(4, 5));
+
+function divide(a, b) {
+  return a * b;
+}
+
+console.log(divide(10, 2));
